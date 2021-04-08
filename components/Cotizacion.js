b3d1781ee5ddaf87945cb0043a7436c5f67d4f60
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-07 18:17:25 -0600

    enviando la cotizacion al componente

--- components/Cotizacion.js
+++ components/Cotizacion.js
@@ -1,5 +1,14 @@
-const Cotizacion = () => {
-    return (  );
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const Cotizacion = ({ resultado }) => {
+
+    if ( Object.keys( resultado ).length === 0 ) return null;
+
+    return ( 
+        <Text>El precio del bitcoin : { resultado.PRICE }</Text>
+     );
 }
  
 export default Cotizacion;
